20aef314bb1918fec07a656e67fc884b82afae47
robertojmm <robertojmunozdeleon@gmail.com>
2019-11-10 23:37:32 +0100

    feat(pizza.model): Added a few more properties to allow "polymorphism"

--- src/models/Pizza.model.js
+++ src/models/Pizza.model.js
@@ -1,8 +1,19 @@
 class Pizza {
-  constructor({ name, baseSize, price, ingredients = {} }) {
+  constructor({ name, image = '', baseSize, price = 0, ingredients = {} }) {
     this.name = name;
+    this.image = image;
     this.baseSize = baseSize;
     this.price = price;
+    this.amount = 1;
     this.ingredients = ingredients;
   }
+
+  toString = () => `${this.name}-${this.baseSize}-${this.price}`;
+
+  calculatePrice = () => {
+    //test this
+    this.price = this.ingredients.reduce((totalPrice, ingredientPrice) => {
+      return totalPrice + ingredientPrice;
+    }, 0);
+  };
 }
