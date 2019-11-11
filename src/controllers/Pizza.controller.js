886855b28c7d94216818f18c5c1b3aa3b9056cce
trampitax <trampitaxonfire@gmail.com>
2019-11-11 14:04:40 +0100

    feat(pizza.controller): Added handler calculateTotalPrice

--- src/controllers/Pizza.controller.js
+++ src/controllers/Pizza.controller.js
@@ -6,20 +6,19 @@ class PizzaController {
 
     this.view.bindLoadJson(this.handlerLoadJson);
     this.view.bindAddPizzaToShoppingCart(this.handlerAddPizzaToShoppingCart);
-    this.view.bindDeletePizzaFromShoppingCart(
-      this.handlerDeletePizzaFromShoppingCart
-    );
+    this.view.bindDeletePizzaFromShoppingCart(this.handlerDeletePizzaFromShoppingCart);
 
     this.view.bindJsonToPizzaModel(this.handlerJsonToPizzaModel);
+    this.view.bindCalculateTotalPrice(this.handlerCalculateTotalPrice);
   }
 
   handlerLoadJson = path => this.pizzaService.loadJson(path);
 
-  handlerAddPizzaToShoppingCart = (pizza, shoppingCart) =>
-    this.shoppingCartService.addPizzaToShoppingCart(pizza, shoppingCart);
+  handlerAddPizzaToShoppingCart = (pizza, shoppingCart) => this.shoppingCartService.addPizzaToShoppingCart(pizza, shoppingCart);
 
-  handlerDeletePizzaFromShoppingCart = (pizza, shoppingCart) =>
-    this.shoppingCartService.deletePizzaFromShoppingCart(pizza, shoppingCart);
+  handlerDeletePizzaFromShoppingCart = (pizza, shoppingCart) => this.shoppingCartService.deletePizzaFromShoppingCart(pizza, shoppingCart);
 
   handlerJsonToPizzaModel = json => this.pizzaService.jsonToPizzaModel(json);
+
+  handlerCalculateTotalPrice = shoppingCart => this.shoppingCartService.calculateTotalPrice(shoppingCart);
 }
