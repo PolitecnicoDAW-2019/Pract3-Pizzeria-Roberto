05833d6d251caf4f2a0e52a5bec916154d0863c8
trampitax <trampitaxonfire@gmail.com>
2019-11-11 14:05:39 +0100

    style(pizza.service): Changed function to one line function

--- src/services/Pizza.service.js
+++ src/services/Pizza.service.js
@@ -2,11 +2,15 @@ class PizzaService {
   loadJson = jsonPath => fetch(jsonPath).then(response => response.json());
 
   jsonToPizzaModel = json => {
+    return json.map(({ name, image, prices }) => Object.entries(prices).map(([size, value]) => new Pizza({ name: name, image: image, baseSize: size, price: value })));
+  };
+
+  /*jsonToIngredientModel = json => {
     return json.map(({ name, image, prices }) =>
       Object.entries(prices).map(
         ([size, value]) =>
           new Pizza({ name: name, image: image, baseSize: size, price: value })
       )
     );
-  };
+  }*/
 }
