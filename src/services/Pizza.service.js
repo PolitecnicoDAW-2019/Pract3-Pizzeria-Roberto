f2c273911370704ed99a7283cb295ea80b2c3670
robertojmm <robertojmunozdeleon@gmail.com>
2019-11-11 21:33:04 +0100

    feat(pizza.service): Added json parse to ingredient model

--- src/services/Pizza.service.js
+++ src/services/Pizza.service.js
@@ -2,15 +2,21 @@ class PizzaService {
   loadJson = jsonPath => fetch(jsonPath).then(response => response.json());
 
   jsonToPizzaModel = json => {
-    return json.map(({ name, image, prices }) => Object.entries(prices).map(([size, value]) => new Pizza({ name: name, image: image, baseSize: size, price: value })));
-  };
-
-  /*jsonToIngredientModel = json => {
     return json.map(({ name, image, prices }) =>
       Object.entries(prices).map(
         ([size, value]) =>
           new Pizza({ name: name, image: image, baseSize: size, price: value })
       )
     );
-  }*/
+  };
+
+  jsonToIngredientModel = json => {
+    return Object.entries(json).map(
+      ([name, { basePrice, size }]) => new Ingredient(name, basePrice, size)
+    );
+  };
 }
+/*Object.entries(prices).map(
+        ([size, value]) =>
+          new Pizza({ name: name, image: image, baseSize: size, price: value })
+      )*/
