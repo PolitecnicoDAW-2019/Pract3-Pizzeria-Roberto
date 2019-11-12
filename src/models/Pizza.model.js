f957ed604f0484b4a939f970267e2e35e90ff8ca
robertojmm <robertojmunozdeleon@gmail.com>
2019-11-12 20:23:18 +0100

    feat(pizza.model): Changed the model to store the multipliers. Also added the necessary functions to calcultate the total price of the pizza

--- src/models/Pizza.model.js
+++ src/models/Pizza.model.js
@@ -1,17 +1,38 @@
 class Pizza {
-  constructor({ name, image = '', baseSize, price = 0, ingredients = {} }) {
+  constructor({ name, baseSize, image = '', price = 0, ingredients = {} }) {
     this.name = name;
-    this.image = image;
     this.baseSize = baseSize;
+    this.image = image;
     this.price = price;
     this.amount = 1;
     this.ingredients = ingredients;
+    this.multiplier = this.setMultiplier();
   }
 
+  setMultiplier = () => {
+    const multipliers = {
+      small: 1,
+      medium: 1.1,
+      large: 1.2
+    };
+
+    return multipliers[this.baseSize];
+  };
+
   calculatePrice = () => {
-    //test this
-    this.price = this.ingredients.reduce((totalPrice, ingredientPrice) => {
-      return totalPrice + ingredientPrice;
-    }, 0);
+    const totalIngredients = this.ingredients.reduce(
+      (totalPrice, ingredientPrice) => {
+        return totalPrice + this.multiplier * ingredientPrice;
+      },
+      0
+    );
+
+    const basePrices = {
+      small: 3,
+      medium: 5,
+      large: 6
+    };
+
+    return totalIngredients + basePrices[this.baseSize];
   };
 }
