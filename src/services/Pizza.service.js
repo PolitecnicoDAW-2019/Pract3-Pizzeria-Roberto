9d89f99cd7461dc4892540a5db500da25353b462
robertojmm <robertojmunozdeleon@gmail.com>
2019-11-12 20:24:47 +0100

    refactor(pizza.service): Removed the function to parse a Json to ingredient model

--- src/services/Pizza.service.js
+++ src/services/Pizza.service.js
@@ -5,18 +5,8 @@ class PizzaService {
     return json.map(({ name, image, prices }) =>
       Object.entries(prices).map(
         ([size, value]) =>
-          new Pizza({ name: name, image: image, baseSize: size, price: value })
+          new Pizza({ name: name, baseSize: size, image: image, price: value })
       )
     );
   };
-
-  jsonToIngredientModel = json => {
-    return Object.entries(json).map(
-      ([name, { basePrice, size }]) => new Ingredient(name, basePrice, size)
-    );
-  };
 }
-/*Object.entries(prices).map(
-        ([size, value]) =>
-          new Pizza({ name: name, image: image, baseSize: size, price: value })
-      )*/
