0ebeb99abbbb1f558048ed7f5099529a6b034b51
robertojmm <robertojmunozdeleon@gmail.com>
2019-11-12 20:20:00 +0100

    feat(constants): Added an "enum" object to determinate the pizza size

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,2 +1,7 @@
 const PRECONFIGUREDPIZZAS_PATH = '/src/data/preconfiguredPizzas.json';
 const INGREDIENTS_PATH = '/src/data/ingredients.json';
+const Sizes = {
+  small: 'small',
+  medium: 'medium',
+  large: 'large'
+};
